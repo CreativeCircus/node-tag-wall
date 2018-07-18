ae6bf27d25163d6fe1bbf7e26d1c64d829705ba4
Christopher Silich <chrissilich@gmail.com>
2018-07-18 12:53:38 -0400

    ready for class

---
+++ front/dist/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// when the user clicks the button
+document.querySelector('button.tagit').addEventListener("click", function () {
+
+	// send the tag to the server
+
+});
+//# sourceMappingURL=main.js.map
---
+++ front/src/js/main.js
@@ -0,0 +1,8 @@
+
+
+// when the user clicks the button
+document.querySelector('button.tagit').addEventListener("click", function() {
+
+	// send the tag to the server
+
+})
